0e775a5857e96cb4a932f52796fec096a47afe25
dmitrpet <dmitrpet05@gmail.com>
2023-07-11 22:30:30 +0500

    Add criteria operator annotations

---
+++ src/main/java/com/github/dmitrpet/criteriagenerator/annotations/operators/GtOperator.java
@@ -0,0 +1,13 @@
+package com.github.dmitrpet.criteriagenerator.annotations.operators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface GtOperator {
+    String targetFieldName() default "";
+    String group() default "";
+}
