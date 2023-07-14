0c7d583f28352f3c7eecf101182de843e363be37
dmitrpet <dmitrpet05@gmail.com>
2023-07-14 22:38:31 +0500

    Add "AND" and "OR" operators

---
+++ src/main/java/com/github/dmitrpet/criteriagenerator/annotations/operators/AndOperator.java
@@ -0,0 +1,12 @@
+package com.github.dmitrpet.criteriagenerator.annotations.operators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface AndOperator {
+    String group();
+}
